4e8795dc74f407330c6e3b2e6810bde0bda323bc
Daniel Lee <bearlee@alum.mit.edu>
2013-06-11 16:29:12 -0400

    adding a websocket exposed linear model

---
+++ src/websocket_linear_model.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <models/linear_model.hpp>
+
+int main(int argc, const char* argv[]) {
+  std::cout 
+    << "------------------------------------------------------------" 
+    << std::endl;
+
+  
+  
+  std::cout 
+    << "------------------------------------------------------------" 
+    << std::endl;
+  return 0;
+}
