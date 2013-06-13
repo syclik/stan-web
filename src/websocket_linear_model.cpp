8d1a56c3f4ca95a7c1fcbc4834f5b45d7ca36354
Daniel Lee <bearlee@alum.mit.edu>
2013-06-13 18:10:27 -0400

    updating

--- src/websocket_linear_model.cpp
+++ src/websocket_linear_model.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <models/linear_model.hpp>
-#include <Wt/WApplication>
 
 // typedef linear_model_model_namespace::linear_model_model Model;
 // typedef boost::ecuyer1988 rng_t;
@@ -92,115 +91,356 @@
 //   return return_code;
 // }
 
+// This may look like C code, but it's really -*- C++ -*-
+/*
+ * Copyright (C) 2008 Emweb bvba, Heverlee, Belgium.
+ *
+ * See the LICENSE file for terms of use.
+ */
+#ifndef SIMPLECHATSERVER_H_
+#define SIMPLECHATSERVER_H_
 
-#include <Wt/WApplication>
-#include <Wt/WContainerWidget>
-#include <Wt/WPushButton>
-#include <Wt/WProgressBar>
+#include <boost/noncopyable.hpp>
 
-#include <iostream>
+#include <Wt/WSignal>
+#include <Wt/WString>
+
+namespace Wt {
+  class WServer;
+}
+
+#include <set>
+#include <map>
 #include <boost/thread.hpp>
 
-/*
- * This is a minimal server push example, which is used to update the GUI
- * while a big work is computing in another thread.
- *
- * This example grabs the userinterface UpdateLock to directly modify
- * an application's user-interface from a worker thread. This works
- * fine for a thread that was created and is owned by a single
- * application, doing work involving only that application.
- *
- * In more complex scenarios, it may be easier to use WServer::post()
- * to post an event to a session. This approach is illustrated in the
- * simplechat example.
+/**
+ * @addtogroup chatexample
  */
-class BigWorkWidget : public Wt::WContainerWidget
+/*@{*/
+
+/*! \brief Encapsulate a chat event.
+ */
+class ChatEvent
 {
 public:
-  BigWorkWidget(Wt::WContainerWidget *parent)
-    : WContainerWidget(parent)
-  {
-    startButton_ = new Wt::WPushButton("Start", this);
-    startButton_->clicked().connect(startButton_, &Wt::WPushButton::disable);
-    startButton_->clicked().connect(this, &BigWorkWidget::startBigWork);
-    startButton_->setMargin(2);
+  /*! \brief Enumeration for the event type.
+   */
+  enum Type { Login, Logout, Rename, Message };
 
-    progress_ = new Wt::WProgressBar(this);
-    progress_->setInline(false);
-    progress_->setMinimum(0);
-    progress_->setMaximum(20);
-    progress_->setMargin(2);
-  }
+  /*! \brief Get the event type.
+   */
+  Type type() const { return type_; }
 
-  virtual ~BigWorkWidget() {
-    workThread_.join();
-  }
+  /*! \brief Get the user who caused the event.
+   */
+  const Wt::WString& user() const { return user_; }
+
+  /*! \brief Get the message of the event.
+   */
+  const Wt::WString& message() const { return message_; }
+
+  /*! \brief Get the extra data for this event.
+   */
+  const Wt::WString& data() const { return data_; }
+
+  /*! \brief Get the message formatted as HTML, rendered for the given user.
+   *
+   * The \p format indicates how the message should be formatted.
+   */
+  const Wt::WString formattedHTML(const Wt::WString& user,
+				  Wt::TextFormat format) const;
 
 private:
-  Wt::WPushButton *startButton_;
-  Wt::WProgressBar *progress_;
-
-  boost::thread workThread_;
-
-  void startBigWork() {
-    Wt::WApplication *app = Wt::WApplication::instance();
-
-    // Enable server push
-    app->enableUpdates(true);
-
-    workThread_ 
-      = boost::thread(boost::bind(&BigWorkWidget::doBigWork, this, app));
-
-    progress_->setValue(0);
-    startButton_->setText("Busy...");
-  }
+  Type type_;
+  Wt::WString user_;
+  Wt::WString data_;
+  Wt::WString message_;
 
   /*
-   * This function runs from another thread.
-   *
-   * From within this thread, we cannot use WApplication::instance(),
-   * since that use thread-local storage. We can only access
-   * WApplication::instance() after we have grabbed its update-lock.
+   * Both user and html will be formatted as html
    */
-  void doBigWork(Wt::WApplication *app)
-  {
-    for (unsigned i = 0; i < 20; ++i) {
-      // Do 50 ms of hard work.
-      boost::this_thread::sleep(boost::posix_time::milliseconds(50));
+  ChatEvent(const Wt::WString& user, const Wt::WString& message)
+    : type_(Message), user_(user), message_(message)
+  { }
 
-      // Get the application update lock to update the user-interface
-      // with a progress indication.
-      Wt::WApplication::UpdateLock uiLock(app);
-      if (uiLock) {
-	progress_->setValue(i + 1);
-	app->triggerUpdate();
-      }
-    }
+  ChatEvent(Type type, const Wt::WString& user,
+	    const Wt::WString& data = Wt::WString::Empty)
+    : type_(type), user_(user), data_(data)
+  { }
 
-    Wt::WApplication::UpdateLock uiLock(app);
-
-    if (uiLock) {
-      startButton_->enable();
-      startButton_->setText("Again!");
-
-      app->triggerUpdate();
-
-      // Disable server push
-      app->enableUpdates(false);
-    }
-  }
+  friend class SimpleChatServer;
 };
 
-Wt::WApplication *createApplication(const Wt::WEnvironment& env)
-{
-  Wt::WApplication *app = new Wt::WApplication(env);
-  app->setCssTheme("polished");
-  new BigWorkWidget(app->root());
+typedef boost::function<void (const ChatEvent&)> ChatEventCallback;
 
-  return app;
+/*! \brief A simple chat server
+ */
+class SimpleChatServer : boost::noncopyable
+{
+public:
+  /*
+   * A reference to a client.
+   */
+  class Client
+  {
+  };
+
+  /*! \brief Create a new chat server.
+   */
+  SimpleChatServer(Wt::WServer& server);
+
+  /*! \brief Connects to the chat server.
+   *
+   * The passed callback method is posted to when a new chat event is
+   * received.
+   *
+   * Returns whether the client has been connected (or false if the client
+   * was already connected).
+   */
+  bool connect(Client *client, const ChatEventCallback& handleEvent);
+
+  /*! \brief Disconnect from the chat server.
+   *
+   * Returns whether the client has been disconnected (or false if the client
+   * was not connected).
+   */  
+  bool disconnect(Client *client);
+
+  /*! \brief Try to login with given user name.
+   *
+   * Returns false if the login was not successful.
+   */
+  bool login(const Wt::WString& user);
+
+  /*! \brief Logout from the server.
+   */
+  void logout(const Wt::WString& user);
+
+  /*! \brief Changes the name.
+   */
+  bool changeName(const Wt::WString& user, const Wt::WString& newUser);
+
+  /*! \brief Get a suggestion for a guest user name.
+   */
+  Wt::WString suggestGuest();
+
+  /*! \brief Send a message on behalve of a user.
+   */
+  void sendMessage(const Wt::WString& user, const Wt::WString& message);
+
+  /*! \brief Typedef for a collection of user names.
+   */
+  typedef std::set<Wt::WString> UserSet;
+
+  /*! \brief Get the users currently logged in.
+   */
+  UserSet users();
+
+private:
+  struct ClientInfo {
+    std::string sessionId;
+    ChatEventCallback eventCallback;
+  };
+
+  typedef std::map<Client *, ClientInfo> ClientMap;
+
+  Wt::WServer& server_;
+  boost::recursive_mutex mutex_;
+  ClientMap clients_;
+  UserSet users_;
+
+  void postChatEvent(const ChatEvent& event);
+};
+
+/*@}*/
+
+#endif // SIMPLECHATSERVER_H_
+
+
+/*
+ * Copyright (C) 2008 Emweb bvba, Heverlee, Belgium.
+ *
+ * See the LICENSE file for terms of use.
+ */
+
+#include <Wt/WServer>
+
+#include <iostream>
+#include <boost/lexical_cast.hpp>
+
+using namespace Wt;
+
+const WString ChatEvent::formattedHTML(const WString& user,
+				       TextFormat format) const
+{
+  switch (type_) {
+  case Login:
+    return WString::fromUTF8("<span class='chat-info'>")
+      + WWebWidget::escapeText(user_) + " joined.</span>";
+  case Logout:
+    return WString::fromUTF8("<span class='chat-info'>")
+      + ((user == user_) ?
+	 WString::fromUTF8("You") :
+	 WWebWidget::escapeText(user_))
+      + " logged out.</span>";
+  case Rename:
+    return "<span class='chat-info'>"
+      + ((user == data_ || user == user_) ?
+	 "You are" :
+	 (WWebWidget::escapeText(user_) + " is")) 
+      + " now known as " + WWebWidget::escapeText(data_) + ".</span>";
+  case Message:{
+    WString result;
+
+    result = WString("<span class='")
+      + ((user == user_) ?
+	 "chat-self" :
+	 "chat-user")
+      + "'>" + WWebWidget::escapeText(user_) + ":</span>";
+
+    WString msg
+      = (format == XHTMLText ? message_ : WWebWidget::escapeText(message_));
+
+    if (message_.toUTF8().find(user.toUTF8()) != std::string::npos)
+      return result + "<span class='chat-highlight'>" + msg + "</span>";
+    else
+      return result + msg;
+  }
+  default:
+    return "";
+  }
 }
 
-int main(int argc, char **argv)
+
+SimpleChatServer::SimpleChatServer(WServer& server)
+  : server_(server)
+{ }
+
+bool SimpleChatServer::connect(Client *client,
+			       const ChatEventCallback& handleEvent)
 {
-  return WRun(argc, argv, &createApplication);
+  boost::recursive_mutex::scoped_lock lock(mutex_);
+
+  if (clients_.count(client) == 0) {
+    ClientInfo clientInfo;
+  
+    clientInfo.sessionId = WApplication::instance()->sessionId();
+    clientInfo.eventCallback = handleEvent;
+
+    clients_[client] = clientInfo;
+
+    return true;
+  } else
+    return false;
 }
+
+bool SimpleChatServer::disconnect(Client *client)
+{
+  boost::recursive_mutex::scoped_lock lock(mutex_);
+
+  return clients_.erase(client) == 1;
+}
+
+bool SimpleChatServer::login(const WString& user)
+{
+  boost::recursive_mutex::scoped_lock lock(mutex_);
+  
+  if (users_.find(user) == users_.end()) {
+    users_.insert(user);
+
+    postChatEvent(ChatEvent(ChatEvent::Login, user));
+
+    return true;
+  } else
+    return false;
+}
+
+void SimpleChatServer::logout(const WString& user)
+{
+  boost::recursive_mutex::scoped_lock lock(mutex_);
+
+  UserSet::iterator i = users_.find(user);
+
+  if (i != users_.end()) {
+    users_.erase(i);
+
+    postChatEvent(ChatEvent(ChatEvent::Logout, user));
+  }
+}
+
+bool SimpleChatServer::changeName(const WString& user, const WString& newUser)
+{
+  if (user == newUser)
+    return true;
+
+  boost::recursive_mutex::scoped_lock lock(mutex_);
+  
+  UserSet::iterator i = users_.find(user);
+
+  if (i != users_.end()) {
+    if (users_.count(newUser) == 0) {
+      users_.erase(i);
+      users_.insert(newUser);
+
+      postChatEvent(ChatEvent(ChatEvent::Rename, user, newUser));
+
+      return true;
+    } else
+      return false;
+  } else
+    return false;
+}
+
+WString SimpleChatServer::suggestGuest()
+{
+  boost::recursive_mutex::scoped_lock lock(mutex_);
+
+  for (int i = 1;; ++i) {
+    std::string s = "guest " + boost::lexical_cast<std::string>(i);
+    WString ss = s;
+
+    if (users_.find(ss) == users_.end())
+      return ss;
+  }
+}
+
+void SimpleChatServer::sendMessage(const WString& user, const WString& message)
+{
+  postChatEvent(ChatEvent(user, message));
+}
+
+void SimpleChatServer::postChatEvent(const ChatEvent& event)
+{
+  boost::recursive_mutex::scoped_lock lock(mutex_);
+
+  WApplication *app = WApplication::instance();
+
+  for (ClientMap::const_iterator i = clients_.begin(); i != clients_.end();
+       ++i) {
+    /*
+     * If the user corresponds to the current application, we directly
+     * call the call back method. This avoids an unnecessary delay for
+     * the update to the user causing the event.
+     *
+     * For other uses, we post it to their session. By posting the
+     * event, we avoid dead-lock scenarios, race conditions, and
+     * delivering the event to a session that is just about to be
+     * terminated.
+     */
+    if (app && app->sessionId() == i->second.sessionId)
+      i->second.eventCallback(event);
+    else
+      server_.post(i->second.sessionId,
+		   boost::bind(i->second.eventCallback, event));
+  }
+}
+
+SimpleChatServer::UserSet SimpleChatServer::users()
+{
+  boost::recursive_mutex::scoped_lock lock(mutex_);
+
+  UserSet result = users_;
+
+  return result;
+}
+
