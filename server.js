b4a6454dfc33fd5d4d9352e804d9e100534c5f80
Daniel Lee <bearlee@alum.mit.edu>
2013-08-29 14:37:51 -0400

    adding call to linear model in server.js

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
+var addon = require('./build/Release/linear_model');
+var linear_model = new addon.LinearModel(100);
 
 
 app.get('/hello.txt', function(req, res){
-  res.send('Hello World');
+  res.send(linear_model.sample());
 });
 
 app.listen(3000);
